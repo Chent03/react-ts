e4dff7dbafb3994351551db7a446261659c14d65
Tony Chen <tochen123@gmail.com>
2018-08-12 22:06:59 -0400

    Got React with typescript, webpack4, scss working

---
+++ src/components/App.tsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+
+export class App extends React.Component<IProps, IState> {
+    render() {
+        return <h1>Hello, {this.props.name}</h1>;
+    }
+}
+
+interface IProps {
+    name: string;
+}
+
+interface IState {}
